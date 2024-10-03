0de6ecae9c2bad3dafeb01e89ff464beff4aceb8
ElaineFarrugia <elaine.vassallo@mcast.edu.mt>
2024-10-03 13:43:39 +0200

    intro and worksheet 1
    intro and worksheet 1

---
+++ Worksheet1/Worksheet1/Program.cs
@@ -0,0 +1,16 @@
+﻿string sequence = "hdgTG45"; //create sequence
+
+Console.WriteLine("Remember this: "+sequence); //output sequence
+Thread.Sleep(5000);//delay
+
+Console.Clear();
+
+Console.WriteLine("Enter the sequence: ");
+string userInput = Console.ReadLine();
+
+if (sequence == userInput)
+    Console.WriteLine("You guessed!");
+else
+    Console.WriteLine("You did not guess");
+
+
