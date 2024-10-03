7a9d00c6ad44c9f1189bfa2434731ab1410c56c9
ElaineFarrugia <elaine.vassallo@mcast.edu.mt>
2024-10-03 09:52:42 +0200

    Create Program.cs

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,17 @@
+﻿int generatedNumber; //variable declaration
+
+Random generator = new Random();//preparing generator
+generatedNumber = generator.Next(1,11); //get random no. 1-10
+
+Console.WriteLine(generatedNumber); //print generated number
+
+Console.WriteLine("I selected a number 1-10. Can you guess it?");
+
+string userInput = Console.ReadLine(); //get user input
+
+int userInputInt = Convert.ToInt32(userInput); //convert str to int
+
+if (userInputInt == generatedNumber)    //checking if user guessed
+    Console.WriteLine("You guessed!");
+else                                    //otherwise
+    Console.WriteLine("You did not guess");
