f50611e9300c5bc9fab0794a36fd18cf783a5426
Elaine Farrugia <>
2024-10-18 20:40:47 +0200

    Worksheet3

---
+++ Worksheet3/Worksheet3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("What number do you want to countdown from?");
+
+string numStr = Console.ReadLine();
+int num = Convert.ToInt32(numStr);
+
+for(int i = num; i >= 0; i--)
+{
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+}
+//OR
+for (int i = 0; i < num; i++)
+{
+    Console.WriteLine(num-i);
+    Thread.Sleep(1000);
+}
