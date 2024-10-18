970c847017123bfe06353ad59cea178e75334401
Elaine Farrugia <>
2024-10-18 20:41:39 +0200

    forloops

---
+++ loopsdemo/loopsdemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+for (int i = 1; i <= 100; i++)
+{
+    Console.WriteLine("hello "+i);
+}
+
+/*
+Console.WriteLine("Till what number do you want to print?");
+string inputStr = Console.ReadLine();
+int input = Convert.ToInt32(inputStr);
+
+for (int i = 1; i <= input; i++)
+{
+    Console.WriteLine("hello " + i);
+}*/
+
+Console.WriteLine("Multiplication table of which number?");
+string inputStr = Console.ReadLine();
+int input = Convert.ToInt32(inputStr);
+
+for (int i = 1; i <= 10; i++)
+{
+    int result = input * i;
+    Console.WriteLine(input + " * " + i + " = " + result);
+    //OR
+    Console.WriteLine($"{input} * {i} = {result}");
+}
