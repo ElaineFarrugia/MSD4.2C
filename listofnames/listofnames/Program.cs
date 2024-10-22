089d37ef59362b117f190e0f191c91cd64fe2ce3
Elaine Farrugia <>
2024-10-22 14:31:59 +0200

    List of Names

---
+++ listofnames/listofnames/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+List<string> names = new List<string>();
+
+string input;
+
+while (true)
+{
+    Console.WriteLine("Enter a name: ");
+    input = Console.ReadLine();
+
+    if (input == "stop")
+        break;
+
+    names.Add(input);   
+}
+
+foreach (string name in names)
+    Console.WriteLine(name);
+
+
+Random generator = new Random();
+int rand  = generator.Next(0, names.Count);
+
+Console.WriteLine("The lucky winner is: " + names[rand]);
