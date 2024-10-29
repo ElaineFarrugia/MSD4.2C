ac9cc188a454ccac7d2b481cd6dbabd707e9e3f5
Elaine Farrugia <>
2024-10-29 16:55:13 +0100

    strings

---
+++ Demo6Strings/Demo6Strings/Demo6Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter your name and surname");
+string fullName = Console.ReadLine();
+
+int indexOfSpace = fullName.IndexOf(' ');
+
+if (indexOfSpace == -1)
+    Console.WriteLine("No space found!");
+else
+{
+    char firstInitial = fullName[0];
+    char surnameInitial = fullName[indexOfSpace+1];
+
+    Console.WriteLine("Initials: " +
+        firstInitial + "." + surnameInitial + ".");
+
+    string name = fullName.Substring(0, indexOfSpace);
+    string surname = fullName.Substring(indexOfSpace+1);
+
+    Console.WriteLine("Name Only: " + name);
+    Console.WriteLine("Surname Only: " + surname);
+
+    Console.WriteLine("Your name has "+name.Length
+                                     +" letters");
+}
---
+++ Worksheet8/Worksheet8/Worksheet8/Program.cs
@@ -0,0 +1,22 @@
+﻿List<string> wordList = new List<string>
+{
+    "cat", "hat", "bat", "rat", "mat", "flat",
+    "dog", "frog", "log", "jog", "smog",
+    "tree", "free", "see", "bee", "spree",
+    "light", "fight", "night", "sight", "might"
+};
+
+Console.WriteLine("Enter a word: ");
+string input = Console.ReadLine();
+string input_ending = input.Substring(input.Length-2, 2);
+
+foreach (string word in wordList)
+{
+    string word_ending = word.Substring(input.Length - 2, 2);
+    
+    if (input_ending == word_ending)
+    {
+        Console.WriteLine("Found rhyming word: " + word);        
+    }
+}
+
