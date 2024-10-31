2ad5446ed09a060cf641c6b9c0ce55a7f61ee914
Elaine Farrugia <>
2024-10-31 12:12:39 +0100

    Demo 7 - Methods

---
+++ Demo7/Demo7/Demo7/Program.cs
@@ -0,0 +1,56 @@
+﻿List<int> marks = new List<int> ();
+
+while (true)
+{
+    Console.Write("Enter mark: ");
+    string inputStr = Console.ReadLine ();
+    int mark = Convert.ToInt32(inputStr);
+
+    if (mark == -1)
+    {
+        //PrintMarks(marks);
+        break;
+    }
+
+    marks.Add(mark);
+}
+PrintMarks(marks);
+
+Console.WriteLine("Average: "+CalculateAverage(marks));
+//OR
+float average = CalculateAverage(marks);
+Console.WriteLine("Average: " + average);
+
+foreach (int mark in marks)
+    Console.WriteLine(ConvertToGrade(mark));
+
+void PrintMarks(List<int> studentmarks)
+{
+    foreach (int mark in studentmarks)
+        Console.WriteLine(mark);
+}
+
+float CalculateAverage(List<int> studentmarks)
+{
+    int sum = 0;
+    foreach (int mark in studentmarks)
+        sum += mark;
+
+    float avg = sum / studentmarks.Count;
+    return avg;
+}
+
+float ConvertToGrade(int mark)
+{
+    if (mark >= 90)
+        return 'A';
+    else if (mark >= 80)
+        return 'B';
+    else if (mark >= 70)
+        return 'C';
+    else if (mark >= 60)
+        return 'D';
+    else
+        return 'U';
+
+}
