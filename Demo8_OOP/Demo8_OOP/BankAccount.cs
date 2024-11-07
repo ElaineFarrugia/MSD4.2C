1dab73fb187be94ab5206f4dc183c718ff5b9c2d
Elaine Farrugia <>
2024-11-07 10:28:02 +0100

    Demo 8 OOP - Part 1

---
+++ Demo8_OOP/Demo8_OOP/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo8_OOP
+{
+    public class BankAccount
+    {
+        //defining the properties
+        public string AccNum;
+        public string HolderName;
+        public string Pin;
+        public double Balance;
+
+        //behaviour/actions
+        public string GetData()
+        {
+            return "Acc Num: " + AccNum +
+                   " Holder Name: " + HolderName +
+                   " Pin: " + Pin +
+                   " Balance: " + Balance;
+        }
+
+        public void Deposit(double amt)
+        {
+            Balance += amt;
+            //OR
+            //Balance = Balance + amt;
+        }
+    }
+}
---
+++ Demo8_OOP/Demo8_OOP/Program.cs
@@ -0,0 +1,16 @@
+﻿using Demo8_OOP;
+
+BankAccount account1 = new BankAccount();
+account1.AccNum = "1";
+account1.HolderName = "Sam";
+account1.Pin = "1234";
+account1.Balance = 500;
+
+BankAccount account2 = new BankAccount();
+account2.AccNum = "2";
+account2.HolderName = "Mary";
+account2.Pin = "5678";
+account2.Balance = 1000;
+
+Console.WriteLine(account1.GetData());
+Console.WriteLine(account2.GetData());
